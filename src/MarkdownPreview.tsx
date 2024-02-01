a67b134d669141a0be687a695bc8720deaaa87da
HaudinFlorence <haudin.florence@gmail.com>
2024-02-01 19:07:01 +0100

    Replace Remarkable and use mystParse and mystToHtml instead.

--- src/MarkdownPreview.tsx
+++ src/MarkdownPreview.tsx
@@ -1,20 +1,19 @@
-import { Remarkable } from 'remarkable';
+import { mystToHtml } from "myst-to-html";
+import { mystParse } from "myst-parser";
 
 interface IProps {
   markdown: string;
 }
 
-const md = new Remarkable();
-
 function renderMarkdownToHTML(markdown: string) {
-  // This is ONLY safe because the output HTML
-  // is shown to the same user, and because you
-  // trust this Markdown parser to not have bugs.
-  const renderedHTML = md.render(markdown);
-  return {__html: renderedHTML};
+  const tree = mystParse(markdown);
+  const renderedHTML = mystToHtml(tree);
+  return { __html: renderedHTML };
 }
 
 export default function MarkdownPreview(props: IProps) {
   const markup = renderMarkdownToHTML(props.markdown);
-  return <div dangerouslySetInnerHTML={markup} />;
+  return (
+    <div className="markdown-preview-class" dangerouslySetInnerHTML={markup} />
+  );
 }
