33ca0c0bfc7e44506c2105fae82442ac45078e13
HaudinFlorence <haudin.florence@gmail.com>
2024-02-01 19:43:27 +0100

    Update App.tsx and App.css for improving the style of the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import { useState } from 'react';
 import MarkdownPreview from './MarkdownPreview';
+import "./App.css";
+
 
 export default function MarkdownEditor() {
   const [postContent, setPostContent] = useState('_Hello,_ **Markdown**!');
   return (
     <>
       <label>
-        Enter some markdown:
-        <textarea
+        <h1 className='app-title'> Myst react app</h1>
+        <p className='text-instruction-class'>Enter some markdown:</p>
+        <textarea className='textarea-class'
           value={postContent}
           onChange={e => setPostContent(e.target.value)}
         />
       </label>
-      <hr />
+      <label>
+      <p className='text-instruction-class'>After parsing, rendered content looks like:</p>
       <MarkdownPreview markdown={postContent} />
+      </label>
     </>
   );
 }
+
