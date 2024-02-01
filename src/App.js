bf02ed4866166b6c8996642b614cea80e01e4c58
HaudinFlorence <haudin.florence@gmail.com>
2024-02-01 18:46:22 +0100

    Create a new empty react app Remove unused files Update package.json, src/App.js and src/index.js with the contents proposed on react dev using Remarkable.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import MarkdownPreview from './MarkdownPreview.js';
 
-function App() {
+export default function MarkdownEditor() {
+  const [postContent, setPostContent] = useState('_Hello,_ **Markdown**!');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <label>
+        Enter some markdown:
+        <textarea
+          value={postContent}
+          onChange={e => setPostContent(e.target.value)}
+        />
+      </label>
+      <hr />
+      <MarkdownPreview markdown={postContent} />
+    </>
   );
 }
-
-export default App;
