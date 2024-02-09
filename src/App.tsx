1d2823ad7f6e067958469640556e28e0d2883388
HaudinFlorence <haudin.florence@gmail.com>
2024-02-09 17:41:20 +0100

    Try to treat the options for mystParse and for mystToHtml methods and retrieve some style sheets from the myst-theme package (import and use of the theme will be done in the future).

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import { useState } from 'react';
-import MarkdownPreview from './MarkdownPreview';
-import "./App.css";
+import { useState } from "react";
+import MarkdownPreview from "./MarkdownPreview";
+import "./styles/App.css";
+import { defaultOptions } from "myst-parser";
 
 
 export default function MarkdownEditor() {
-  const [postContent, setPostContent] = useState('_Hello,_ **Markdown**!');
+  const initial = '_Hello,_ **Markdown**!'
+  const [postContent, setPostContent] = useState(initial);
+
   return (
     <>
       <label>
-        <h1 className='app-title'> Myst react app</h1>
-        <p className='text-instruction-class'>Enter some markdown:</p>
-        <textarea className='textarea-class'
+        <h1 className="app-title"> Myst react app</h1>
+        <p className="text-instruction-class">Enter some markdown:</p>
+        <textarea
+          className="textarea-class"
           value={postContent}
-          onChange={e => setPostContent(e.target.value)}
+          onChange={(e) => setPostContent(e.target.value)}
         />
       </label>
       <label>
-      <p className='text-instruction-class'>After parsing, rendered content looks like:</p>
-      <MarkdownPreview markdown={postContent} />
+        <p className="text-instruction-class">
+          After parsing, rendered content looks like:
+        </p>
+        <MarkdownPreview markdown={postContent} options={defaultOptions} onChange={()=>setPostContent}/>
       </label>
     </>
   );
 }
-
--- src/MarkdownPreview.tsx
+++ src/MarkdownPreview.tsx
@@ -1,19 +1,53 @@
 import { mystToHtml } from "myst-to-html";
-import { mystParse } from "myst-parser";
+import { AllOptions, mystParse} from "myst-parser";
+import './styles/myst.css';
+import type { Handlers } from 'mdast-util-to-hast';
+
+
+
+
+type Options = Partial<AllOptions>;
+type mystToHtmlOpts = {
+  formatHtml?: boolean;
+  hast?: {
+    clobberPrefix?: 'm-';
+    allowDangerousHtml?: boolean;
+    handlers?: Handlers;
+  };
+  stringifyHtml?: {
+    closeSelfClosing?: boolean;
+    allowDangerousHtml?: boolean;
+  };
+}
+const defaultMystToHtmlOpts: mystToHtmlOpts = {
+    formatHtml: true,
+    hast: {
+      clobberPrefix: 'm-',
+      allowDangerousHtml: true,
+    },
+    stringifyHtml: {
+      closeSelfClosing: true,
+      allowDangerousHtml: true
+    }
+  }
 
 interface IProps {
-  markdown: string;
+  markdown: string
+  options: Options
+  onChange: React.Dispatch<HTMLElement>
 }
 
-function renderMarkdownToHTML(markdown: string) {
-  const tree = mystParse(markdown);
-  const renderedHTML = mystToHtml(tree);
+function renderMarkdownToHTML(markdown: string, treeOptions: Options) {
+  const tree = mystParse(markdown, treeOptions);
+ const renderedHTML = mystToHtml(tree, defaultMystToHtmlOpts);
+ console.log('tree:', JSON.stringify(tree, null, 2));
   return { __html: renderedHTML };
+
 }
 
 export default function MarkdownPreview(props: IProps) {
-  const markup = renderMarkdownToHTML(props.markdown);
+  const markup = renderMarkdownToHTML(props.markdown, props.options);
   return (
-    <div className="markdown-preview-class" dangerouslySetInnerHTML={markup} />
+    <div className="markdown-preview-class" dangerouslySetInnerHTML={markup} onChange={()=>markup}/>
   );
 }
