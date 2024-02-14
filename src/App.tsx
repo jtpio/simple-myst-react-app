d78446f10b54c1a5129c6b9f1b686fe6369e68be
HaudinFlorence <haudin.florence@gmail.com>
2024-02-14 19:08:28 +0100

    Use myst-to-react.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import React from "react";
 import { useState } from "react";
-import MarkdownPreview from "./MarkdownPreview";
-import "./styles/App.css";
-import { defaultOptions } from "myst-parser";
+import "./styles/base.css";
+import { defaultOptions, mystParse } from "myst-parser";
+import { ThemeProvider } from "@myst-theme/providers";
+import { MyST, DEFAULT_RENDERERS } from "myst-to-react";
+import { GenericNode } from "myst-common";
 
+function DefaultComponent({ node }: { node: GenericNode }) {
+  if (!node.children) return <span>{node.value}</span>;
+  return (
+    <ThemeProvider renderers={DEFAULT_RENDERERS}>
+      <MyST ast={node.children} />
+    </ThemeProvider>
+  );
+}
 
 export default function MarkdownEditor() {
-  const initial = '_Hello,_ **Markdown**!'
+  const initial = "_Hello,_ **Markdown**!";
   const [postContent, setPostContent] = useState(initial);
+  const tree = mystParse(postContent, defaultOptions);
 
   return (
     <>
@@ -16,15 +28,26 @@ export default function MarkdownEditor() {
         <textarea
           className="textarea-class"
           value={postContent}
-          onChange={(e) => setPostContent(e.target.value)}
+          onChange={(e) => {
+            setPostContent(e.target.value);
+          }}
         />
       </label>
       <label>
         <p className="text-instruction-class">
           After parsing, rendered content looks like:
         </p>
-        <MarkdownPreview markdown={postContent} options={defaultOptions} onChange={()=>setPostContent}/>
+        <div className="markdown-preview-class">
+          <ul>
+            {tree.children.map((node) => (
+              <li key={node.id}>
+                {<DefaultComponent node={node}></DefaultComponent>}
+              </li>
+            ))}
+          </ul>
+        </div>
       </label>
+      ;
     </>
   );
 }
